22fbd1b0159efb1f4ce20a5f39cf4fa31be1cb00
Kevin Desai <78614503+Kdesai117@users.noreply.github.com>
2021-11-09 18:47:33 -0600

    Update photonMain.java

--- photonMain.java
+++ photonMain.java
@@ -1,21 +1,32 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class photonMain {
+	String[] rows;
+	private static final String url = "jdbc:postgresql://ec2-44-199-26-122.compute-1.amazonaws.com:5432/dfoas6b885fbrt?sslmode=require";
+    private static final String user = "xlsnfmxwycfzie";
+    private static final String password = "5dc1c02da3a4cef71d3cce3edad7a304de4871f278873cfb1ef6b499fb234327";
 
 	public static void main(String args[]) {
-		
 		//initialize frame
 		JFrame frame = new JFrame();
 		frame.setSize(1250, 1000);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//connect to server 
+		connect();
 		//run splash screen on event dispatcher
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					frame.setTitle("Splash Screen");
-					splashScreen splsh = new splashScreen(frame);
+					SplashScreen splsh = new SplashScreen(frame);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -46,4 +57,16 @@ public class photonMain {
 			}
 		});
 	}
+	public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+	
 }
